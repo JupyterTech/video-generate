f0d35879ff7b1475d8e9c9cd8755cec194f9fd17
loyal812 <jh.chan0812@gmail.com>
2024-05-27 21:50:50 -0400

    feat: dalle 3 implementation and update the image generation prompt and auto generate the folder structure feature

--- process.php
+++ process.php
@@ -8,7 +8,30 @@ $promises = array();
 $client = new Client();
 $getID3 = new getID3;
 set_time_limit(1000);
-$apiKey = '';
+$apiKey = 'sk-proj-vrShgTCRZRkiewHxm1U4T3BlbkFJm6aWMICQ3MejTnX91qlJ';
+
+if (!is_dir("output")) {
+    mkdir("output", 0777, true);
+}
+if (!is_dir("./output/image")) {
+    mkdir("./output/image", 0777, true);
+}
+if (!is_dir("./output/audio")) {
+    mkdir("./output/audio", 0777, true);
+}
+if (!is_dir("./output/phrase")) {
+    mkdir("./output/phrase", 0777, true);
+}
+if (!is_dir("./output/image_description")) {
+    mkdir("./output/image_description", 0777, true);
+}
+if (!is_dir("./output/story")) {
+    mkdir("./output/story", 0777, true);
+}
+if (!is_dir("./output/video")) {
+    mkdir("./output/video", 0777, true);
+}
+
 function getAudioDuration($audioFile)
 {
     // Command to get the audio duration using ffprobe
@@ -49,9 +72,9 @@ function generate($imgcount, $audio_name, $image_name)
     unlink("video.mp4");
     return $output_name;
 }
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST['inputText'])) {
     $inputText = $_POST['inputText'];
-
     // Generate text with GPT
     $response = $client->request('POST', 'https://api.openai.com/v1/chat/completions', [
         'headers' => ['Authorization' => 'Bearer ' . $apiKey],
@@ -59,8 +82,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST['inputText'])) {
     ]);
     $generatedText = json_decode($response->getBody())->choices[0]->message->content;
     $text_name = date("Y-m-d-h-i-s") . "-output.txt";
-    file_put_contents("./output/text/$text_name", $inputText);
-    file_put_contents("./output/response/$text_name", $generatedText);
+    file_put_contents("./output/phrase/$text_name", $inputText);
+    file_put_contents("./output/story/$text_name", $generatedText);
+    $content = $generatedText;
+
 
     // Generate voice
     $response = $client->request('POST', 'https://api.openai.com/v1/audio/speech', [
@@ -81,22 +106,28 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST['inputText'])) {
     $image_name = array();
     $imageCount = $duration / 3.5; // Number of images to generate
     $imageCount = (int) $imageCount;
-    $sentences = explode(' ', $generatedText);
-    $totalSentences = count($sentences);
-    $sentencesPerThird = $totalSentences / $imageCount;
-    $sentencesPerThird = (int) $sentencesPerThird;
+
     $results = array();
     $video_name = "";
     $count = 0;
+
+    $addtext = "We need to generate $imageCount images for the above story. Write a description for each image to use for dalle prompt, and each description should be sequential. plz include the tag list about each image. Don't use pronoun such as he, her, his, she, their, they, instead of this use character name. Also don't use emoticon. The start of each image description should start with ###image###";
+
+    $imageDescription = $generatedText . $addtext;
+    $response = $client->request('POST', 'https://api.openai.com/v1/chat/completions', [
+        'headers' => ['Authorization' => 'Bearer ' . $apiKey],
+        'json' => ['model' => 'gpt-4o', 'messages' => [["role" => "system", "content" => "You are a helpful assistant."], ["role" => "user", "content" => $imageDescription]]]
+    ]);
+    $generatedText = json_decode($response->getBody())->choices[0]->message->content;
+    $generatedText = str_replace(["\r\n", "\r", "\n"], '', $generatedText);
+    $sentences = explode("###image", $generatedText);
     for ($i = 0; $i < $imageCount; $i++) {
-        $text = "";
-        $start = $i * $sentencesPerThird;
-        for ($j = $i * $sentencesPerThird; $j < ($i + 1) * $sentencesPerThird; $j++) {
-            $text = $text . trim($sentences[$j]) . '.';
-        }
+        $text = $sentences[$i + 1];
+        $imgDes = date("Y-m-d-h-i-s") . "-image$i.txt";
+        file_put_contents("./output/image_description/$imgDes", $text);
         $promises[] = $client->postAsync('https://api.openai.com/v1/images/generations', [
             'headers' => ['Authorization' => 'Bearer ' . $apiKey],
-            'json' => ['model' => 'dall-e-2', 'prompt' => $text, 'n' => 1]
+            'json' => ['model' => 'dall-e-3', 'prompt' => $text, 'n' => 1]
         ]);
         if (($i + 1) % 5 == 0 || ($i + 1) == $imageCount) {
             $results = Promise\Utils::unwrap($promises);
@@ -116,6 +147,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST['inputText'])) {
         }
     }
     // Create video with FFmpeg
-    echo json_encode(['count' => $imageCount, 'finaltext' => $generatedText, 'videoname' => $video_name, 'audioname' => $audio_name, 'image_name' => $image_name]);
+    echo json_encode(['count' => $imageCount, 'finaltext' => $content, 'videoname' => $video_name, 'audioname' => $audio_name, 'image_name' => $image_name]);
     // Redirect to download or display the video
 }
